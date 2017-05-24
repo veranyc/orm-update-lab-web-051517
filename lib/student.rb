28b7e7581c1042c1d68b5a7d45d568d8c2a16388
veranyc <veranyc@gmail.com>
2017-05-24 09:12:15 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,76 @@
+require "pry"
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(id=nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY, name TEXT, grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = <<-SQL
+        UPDATE students SET
+        name = ?, grade = ? WHERE id = ?
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade, self.id)
+      end
+
+    def self.create(name, grade)
+      student = Student.new(name, grade)
+      student.save
+      student
+    end
+
+    def self.new_from_db(row)
+       self.new(row[0],row[1],row[2])
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+          SELECT * FROM students
+          WHERE name = ?
+          LIMIT 1
+          SQL
+        DB[:conn].execute(sql, name).map do |row|
+          self.new_from_db(row)
+        end.first
+        #since results is an array the .first method will give me the first value (name)
+      end
+
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
